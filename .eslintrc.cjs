e1fcf125a172d1a96aa60c454ce8086a47694593
EvgeniyaYakunina <Yakoonina@gmail.com>
2024-08-08 15:30:03 +0300

    fix ESLint bag

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,20 @@
 module.exports = {
   extends: '@it-incubator/eslint-config',
+  settings: {
+    'import/resolver': {
+      alias: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'], // Укажите расширения файлов, которые вы используете
+        map: [
+          ['@', './src'], // Укажите ваш алиас и путь к нему
+        ],
+      },
+    },
+  },
+  // 'import/no-unresolved': [
+  //   'error',
+  //   {
+  //     ignore: ['^firebase-admin/.+'],
+  //   },
+  // ],
+
 }
